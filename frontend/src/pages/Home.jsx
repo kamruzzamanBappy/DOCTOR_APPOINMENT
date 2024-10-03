f567768d262cc09ac3a227c7b5409ce4a02f0c6b
kamruzzamanBappy <kamruzzamanbappy13@gmail.com>
2024-10-03 23:16:17 +0600

    d-3

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Banner from '../components/Banner'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctors from '../components/TopDoctors'
@@ -9,6 +11,8 @@ const Home = () => {
       <Header />
       <SpecialityMenu />
       <TopDoctors />
+      <Banner />
+      <Footer />
        </div>
   )
 }
